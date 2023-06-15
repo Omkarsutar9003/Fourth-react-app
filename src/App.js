cebbfed50f708e2f53a35309bb8a208a7fbd06a8
Omkar Sutar <ipad.omkarsutar@gmail.com>
2023-06-15 23:19:35 +0530

    todo 1 text

--- src/App.js
+++ src/App.js
@@ -3,63 +3,35 @@ import { useRef, useState } from "react";
 function App() {
   return (
     <>
-      <h1>My project</h1>
-      <ListDemo />
+      <h1>My Todo</h1>
+      <MyTodo />
     </>
   );
 }
 
-function ListDemo() {
-
+function MyTodo() {
   let inputref = useRef();
-  let [list, setList] = useState(["delhi"]);
-  
-  let additem=() => {
-    
-    let inputvalue = inputref.current.value;
-    let newlist = [inputvalue, ...list];
-    setList(newlist);
-    inputref.current.value = "";
+  let [todo, setTodo] = useState({ task: "" });
 
+  let taskhandler = (e) => {
+    let newTodo = { ...todo, task: e.target.value };
+    setTodo(newTodo);
   };
 
+  let addTodoaction = () => {
+    alert(todo.task);
+  };
+           
   return (
     <>
-      <div
-        className="row justify-content-center align-items-center"
-        style={{ height: "95vh" }}
-      >
-        <div className="col-sm-12 col-md-6">
-          <h1>Registration App</h1>
-          <input
-            className="form-control"
-            type="text"
-            placeholder="user name"
-            ref={inputref}
-          />
-
-          <input
-            className="form-control"
-            type="text"
-            placeholder="password"
-            ref={inputref}
-          />
-
-          <input
-            className="form-control"
-            type="text"
-            placeholder="Email"
-            ref={inputref}
-          />
-
-          <input
-            type="button"
-            value="Login"
-            className="w-100"
-            onClick={additem}
-          />
-        </div>
-      </div>
+      <input
+        type="text"
+        placeholder="Enter task"
+        value={todo.task}
+        onChange={taskhandler}
+        className="form-control"
+      />
+      <input type="button" value="Add todo" onClick={addTodoaction} />
     </>
   );
 }
