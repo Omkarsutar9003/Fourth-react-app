2f4526ab1e6fa245b2ad3d4affbc46223921258b
Omkar Sutar <ipad.omkarsutar@gmail.com>
2023-06-15 09:33:12 +0530

    without destrure

--- src/App.js
+++ src/App.js
@@ -5,19 +5,22 @@ function App() {
   return (
     <>
       <h1>Props Demo</h1>
-      <Demo />
-      <Demo />
-    </>
-  )
-};
-
-function Demo()
-{
-  return (
-    <>
-      <h1>hello msg</h1>
+      <Demo username="Omkar" place="Kolhapur" />
+      <Demo username="Nitesh" place="Sangli" />
+      <Demo username="Amit" place="Kolhapur" />
     </>
   );
 };
+
+function Demo(props)
+{
+   console.log("inside function", props);
+  return (
+    <>
+      <h1>{props.username}</h1>
+      <h1>{props.place}</h1>
+    </>
+  );
+}
 
 export default App;
